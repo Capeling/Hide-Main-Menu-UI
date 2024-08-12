952dba8a192161f81286a687fb2710edb0cad1d1
Capeling <113705504+Capeling@users.noreply.github.com>
2024-08-12 18:43:56 +0930

    2.206 update

--- src/main.cpp
+++ src/main.cpp
@@ -3,46 +3,46 @@
 
 using namespace geode::prelude;
 
-class $modify(MenuLayerExt, MenuLayer) {
-
-	bool m_isHidden = false;
-	CCMenuItemToggler* m_eyeButton;
-	CCMenu* m_eyeMenu;
+struct HookMenuLayer : Modify<HookMenuLayer, MenuLayer> {
+	struct Fields {
+		bool m_isHidden = false;
+		CCMenuItemToggler* m_eyeButton;
+		CCMenu* m_eyeMenu;
+	};
 
 	bool init() {
 		if(!MenuLayer::init())
 			return false;
 		auto winSize = CCDirector::sharedDirector()->getWinSize();
 		auto on = CircleButtonSprite::create(CCSprite::createWithSpriteFrameName("eye_open.png"_spr), CircleBaseColor::Green, CircleBaseSize::Small);
-
 		auto offEye = CCSprite::createWithSpriteFrameName("eye_closed.png"_spr);
 		offEye->setOpacity(255 / 2.5);
 		
 		auto off = CircleButtonSprite::create(offEye, CircleBaseColor::Gray, CircleBaseSize::Small);
 		off->setOpacity(255 / 2.5);
 
-		m_eyeButton = CCMenuItemToggler::create(on, off, this, menu_selector(MenuLayerExt::onToggleHide));
-		m_eyeMenu = CCMenu::create();
+		m_fields->m_eyeButton = CCMenuItemToggler::create(on, off, this, menu_selector(HookMenuLayer::onToggleHide));
+		m_fields->m_eyeMenu = CCMenu::create();
 
-		m_eyeMenu->setPosition({winSize.width - 25, winSize.height - 25});
-		m_eyeMenu->setID("hide-ui-menu");
+		m_fields->m_eyeMenu->setPosition({winSize.width - 25, winSize.height - 25});
+		m_fields->m_eyeMenu->setID("hide-ui-menu"_spr);
 
-		m_eyeMenu->addChild(m_eyeButton);
-		addChild(m_eyeMenu);
+		m_fields->m_eyeMenu->addChild(m_fields->m_eyeButton);
+		addChild(m_fields->m_eyeMenu);
 		return true;
 	}	
 
 	void onToggleHide(CCObject* sender) {
-		m_isHidden = !m_isHidden;
-		MenuLayerExt::updateMenu();
+		m_fields->m_isHidden = !m_fields->m_isHidden;
+		HookMenuLayer::updateMenu();
 	}
 
 	void updateMenu() {
-		if(m_isHidden) {
+		if(m_fields->m_isHidden) {
 			for(int i = 0; i < this->getChildrenCount(); i++) {
 				auto node = static_cast<CCNode*>(this->getChildren()->objectAtIndex(i));
 				
-				if(node->getID() != "main-menu-bg" && node->getID() != "hide-ui-menu")
+				if(node->getID() != "main-menu-bg" && node->getID() != "hide-ui-menu"_spr)
 					node->setVisible(false);
 			}
 		} else {
