05bf984cf615305ee0963789e9f5363e45af8f5d
Capeling <113705504+Capeling@users.noreply.github.com>
2024-10-03 01:24:00 +0930

    v1.0.3, you're welcome SMJS

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,6 @@ struct HookMenuLayer : Modify<HookMenuLayer, MenuLayer> {
 	struct Fields {
 		bool m_isHidden = false;
 		CCMenuItemToggler* m_eyeButton;
-		CCMenu* m_eyeMenu;
 	};
 
 	bool init() {
@@ -15,20 +14,19 @@ struct HookMenuLayer : Modify<HookMenuLayer, MenuLayer> {
 			return false;
 		auto winSize = CCDirector::sharedDirector()->getWinSize();
 		auto on = CircleButtonSprite::create(CCSprite::createWithSpriteFrameName("eye_open.png"_spr), CircleBaseColor::Green, CircleBaseSize::Small);
+		
 		auto offEye = CCSprite::createWithSpriteFrameName("eye_closed.png"_spr);
 		offEye->setOpacity(255 / 2.5);
-		
 		auto off = CircleButtonSprite::create(offEye, CircleBaseColor::Gray, CircleBaseSize::Small);
 		off->setOpacity(255 / 2.5);
 
 		m_fields->m_eyeButton = CCMenuItemToggler::create(on, off, this, menu_selector(HookMenuLayer::onToggleHide));
-		m_fields->m_eyeMenu = CCMenu::create();
+		m_fields->m_eyeButton->setID("hide-menu-button"_spr);
+		
+		auto menu = static_cast<CCMenu*>(getChildByID("top-right-menu"));
 
-		m_fields->m_eyeMenu->setPosition({winSize.width - 25, winSize.height - 25});
-		m_fields->m_eyeMenu->setID("hide-ui-menu"_spr);
-
-		m_fields->m_eyeMenu->addChild(m_fields->m_eyeButton);
-		addChild(m_fields->m_eyeMenu);
+		menu->addChild(m_fields->m_eyeButton);
+		menu->updateLayout();
 		return true;
 	}	
 
@@ -38,17 +36,34 @@ struct HookMenuLayer : Modify<HookMenuLayer, MenuLayer> {
 	}
 
 	void updateMenu() {
+		CCObject* node;
+		CCMenu* menu = static_cast<CCMenu*>(getChildByID("top-right-menu"));
 		if(m_fields->m_isHidden) {
-			for(int i = 0; i < this->getChildrenCount(); i++) {
-				auto node = static_cast<CCNode*>(this->getChildren()->objectAtIndex(i));
+			CCARRAY_FOREACH(getChildren(), node) {
+				auto node2 = static_cast<CCNode*>(node);
 				
-				if(node->getID() != "main-menu-bg" && node->getID() != "hide-ui-menu"_spr)
-					node->setVisible(false);
+				if(node2->getID() != "main-menu-bg" && node2->getID() != "top-right-menu") {
+					node2->setVisible(false);
+				}
+			}
+			CCARRAY_FOREACH(menu->getChildren(), node) {
+				auto node2 = static_cast<CCNode*>(node);
+
+				log::debug("{}", node2->getID());
+
+				if(node2->getID() != "hide-menu-button"_spr) {
+					node2->setVisible(false);
+				}
 			}
 		} else {
-			for(int i = 0; i < this->getChildrenCount(); i++) {
-				auto node = static_cast<CCNode*>(this->getChildren()->objectAtIndex(i));
-				node->setVisible(true);
+			CCARRAY_FOREACH(getChildren(), node) {
+				auto node2 = static_cast<CCNode*>(node);
+				node2->setVisible(true);
+			}
+
+			CCARRAY_FOREACH(menu->getChildren(), node) {
+				auto node2 = static_cast<CCNode*>(node);
+				node2->setVisible(true);
 			}
 		}
 	}
